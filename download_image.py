1a5b1d7452bf8f69acbd707d2b5f2f341cd7d4b7
darya-k00 <dasha.kon.060203@gmail.com>
2025-04-02 13:59:14 +0300

    функция save_photo

--- download_image.py
+++ download_image.py
@@ -2,7 +2,17 @@ from os.path import splitext
 from urllib.parse import urlsplit
 import requests
 
+if not os.path.exists('images'):
+    os.makedirs('images')
 
+
+def save_photo_in_folder(file_name, image_link, image_number, *args):
+    response = requests.get(image_link, *args)
+    response.raise_for_status()
+
+    with open(f'images/{file_name}_{image_number}.jpg', 'wb') as file:
+        file.write(response.content)
+        
 def download_image(url, directory, params=None):
     response = requests.get(url, params=params)
     response.raise_for_status()
